431c7688dcadd268e526e450223eee2e7efbfe30
Lucca-souza <luccapdesouza@gmail.com>
2025-04-16 15:52:17 -0300

    primeiro codigo

---
+++ index.js
@@ -0,0 +1,44 @@
+const readline = require('readline');
+
+// Variáveis
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let nome;
+let xp;
+let nivel;
+
+// Entrada de dados
+rl.question("Digite o nome do herói: ", (inputNome) => {
+    nome = inputNome;
+
+    rl.question("Digite a quantidade de experiência (XP) do herói: ", (inputXp) => {
+        xp = parseInt(inputXp);
+
+        // Estrutura de decisão
+        if (xp < 1000) {
+            nivel = "Ferro";
+        } else if (xp <= 2000) {
+            nivel = "Bronze";
+        } else if (xp <= 5000) {
+            nivel = "Prata";
+        } else if (xp <= 7000) {
+            nivel = "Ouro";
+        } else if (xp <= 8000) {
+            nivel = "Platina";
+        } else if (xp <= 9000) {
+            nivel = "Ascendente";
+        } else if (xp <= 10000) {
+            nivel = "Imortal";
+        } else {
+            nivel = "Radiante";
+        }
+
+        // Saída
+        console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+        rl.close();
+    });
+});
+
